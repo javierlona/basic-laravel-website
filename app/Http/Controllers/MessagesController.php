ecce614a81e12ba314bca8408703517df5c2a69d
Javier Lona <javierlona1@gmail.com>
2018-08-01 21:11:38 -0500

    contact form inserts data to db

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// to use the message model include it
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -13,6 +15,17 @@ class MessagesController extends Controller
         'name' => 'required',
         'email' => 'required'
       ]);
-      return 'SUCCESS';
+      // create new message using Eloquent
+      $message = new Message;
+      $message->name = $request->input('name');
+      $message->email = $request->input('email');
+      $message->message = $request->input('message');
+      // save message
+      $message->save();
+      /* redirect after entering into DB
+         use with to display a flash message
+         displays it in views > inc > messages
+      */
+      return redirect('/')->with('success', 'Message Sent');
     }
 }
